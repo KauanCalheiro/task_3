30d0eb3c3785aaf118fe15d4d76d046da53513ca
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2024-10-31 12:07:40 -0300

    feat: adicionar suporte a ambientes de desenvolvimento e produção com Docker

--- auth/afterbuild-dev.sh
+++ auth/afterbuild-dev.sh
@@ -5,8 +5,10 @@ cd /var/www/html
 chown -R www-data:www-data /var/www/html
 chmod -R 777 /var/www/html
 
+composer install
+
 cp .env.example .env
+
 php artisan key:generate
-php artisan migrate
 
 apache2-foreground
