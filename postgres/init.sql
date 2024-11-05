aff4efdc9f1edc6e86aeab845584677707b41a9f
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2024-11-05 10:19:02 -0300

    feat: adicionar tabelas para eventos, inscrições, presenças, certificados e e-mails no banco de dados

--- postgres/init.sql
+++ postgres/init.sql
@@ -21,6 +21,59 @@ CREATE TABLE IF NOT EXISTS "auths" (
     "deleted_at" TIMESTAMP
 );
 
+CREATE TABLE IF NOT EXISTS "events" (
+    "id" SERIAL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "dt_init" TIMESTAMP NOT NULL,
+    "dt_end" TIMESTAMP NOT NULL,
+    "location" TEXT,
+    "capacity" INTEGER NOT NULL,
+
+    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP,
+    "deleted_at" TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "inscriptions" (
+    "id" SERIAL PRIMARY KEY,
+    "ref_user" INTEGER NOT NULL,
+    "ref_event" INTEGER NOT NULL,
+    "dt_inscription" TIMESTAMP NOT NULL DEFAULT NOW(),
+
+    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP,
+    "deleted_at" TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "presences" (
+    "id" SERIAL PRIMARY KEY,
+    "ref_inscription" INTEGER NOT NULL,
+    "dt_presence" TIMESTAMP NOT NULL DEFAULT NOW(),
+
+    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP,
+    "deleted_at" TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "certificates" (
+    "id" SERIAL PRIMARY KEY,
+    "ref_inscription" INTEGER NOT NULL,
+    "server_path" TEXT NOT NULL,
+    "validation_code" TEXT NOT NULL,
+
+    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP,
+    "deleted_at" TIMESTAMP
+);
+
+CREATE TABLE IF NOT EXISTS "emails" (
+    "id" SERIAL PRIMARY KEY,
+    "ref_user" INTEGER NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "dt_send" TIMESTAMP NOT NULL DEFAULT NOW(),
+);
 
 INSERT INTO "users" (
     "id",
