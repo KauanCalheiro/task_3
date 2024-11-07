7474fbe74ea711df1cd5268c8e586cdc7daf33a3
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2024-11-07 20:38:05 -0300

    feat: adicionar variáveis de ambiente para configuração do banco de dados e TrustKey

--- inscription/src/middlewares/TrustKey.ts
+++ inscription/src/middlewares/TrustKey.ts
@@ -2,7 +2,7 @@ import { NextFunction, Request, Response } from "express";
 
 const TrustKey = (req: Request, res: Response, next: NextFunction) => {
     const requestToken = req.headers.authorization?.replace("Bearer ", "");
-    const envToken     = process.env.TRUSTKEY;
+    const envToken     = process.env.TRUS_TKEY;
 
     const isTokensMatch = requestToken === envToken;
 
