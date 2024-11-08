4f2c1ff8f10561a7c00e4de9238eca8cc003d1ba
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2024-11-08 11:43:04 -0300

    feat: adicionar middleware RequestLog para registro de requisições na API

--- user/routes/api.php
+++ user/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\UserController;
+use App\Http\Middleware\RequestLog;
 use App\Http\Middleware\TrustKey;
 
 Route::get('/status', function () {
@@ -8,13 +9,15 @@ Route::get('/status', function () {
 });
 
 Route::prefix('/')->group(function () {
-    Route::middleware(TrustKey::class)->group(function () {
-        Route::get   ('',     [UserController::class, 'index'  ]);
-        Route::get   ('{id}', [UserController::class, 'show'   ]);
-        Route::post  ('',     [UserController::class, 'store'  ]);
-        Route::put   ('{id}', [UserController::class, 'update' ]);
-        Route::delete('{id}', [UserController::class, 'destroy']);
+    Route::middleware(RequestLog::class)->group(function () {
+        Route::middleware(TrustKey::class)->group(function () {
+            Route::get   ('',     [UserController::class, 'index'  ]);
+            Route::get   ('{id}', [UserController::class, 'show'   ]);
+            Route::post  ('',     [UserController::class, 'store'  ]);
+            Route::put   ('{id}', [UserController::class, 'update' ]);
+            Route::delete('{id}', [UserController::class, 'destroy']);
 
-        Route::get   ('{id}/login', [UserController::class, 'login'  ]);
+            Route::get   ('{id}/login', [UserController::class, 'login'  ]);
+        });
     });
 });
