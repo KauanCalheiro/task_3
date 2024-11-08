d38710a6cdb454a7ff2d64943579dce2e8027ba5
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2024-11-07 22:34:50 -0300

    fix: renomear tabela 'attendance' para 'attendances' e adicionar novas inserções no script de inicialização do PostgreSQL

--- postgres/init.sql
+++ postgres/init.sql
@@ -46,7 +46,7 @@ CREATE TABLE IF NOT EXISTS "inscriptions" (
     "deleted_at" TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS "attendance" (
+CREATE TABLE IF NOT EXISTS "attendances" (
     "id" SERIAL PRIMARY KEY,
     "ref_inscription" INTEGER NOT NULL,
     "dt_presence" TIMESTAMP NOT NULL DEFAULT NOW(),
@@ -73,6 +73,10 @@ CREATE TABLE IF NOT EXISTS "emails" (
     "title" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "dt_send" TIMESTAMP NOT NULL DEFAULT NOW(),
+
+    "created_at" TIMESTAMP NOT NULL DEFAULT NOW(),
+    "updated_at" TIMESTAMP,
+    "deleted_at" TIMESTAMP
 );
 
 INSERT INTO "users" (
@@ -81,10 +85,49 @@ INSERT INTO "users" (
     "email",
     "password",
     "dt_birth"
-) VALUES (1,
+) VALUES (
+    1,
     'Admin',
     'admin@admin.com',
     md5('admin'),
     '2000-01-01'
 )
 ON CONFLICT DO NOTHING;
+
+INSERT INTO "events" (
+    "id",
+    "name",
+    "description",
+    "dt_init",
+    "dt_end",
+    "location",
+    "capacity"
+) VALUES (
+    1,
+    'Evento Teste',
+    'Evento de teste',
+    '2021-01-01 00:00:00',
+    '2021-01-01 23:59:59',
+    'Local de teste',
+    100
+)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO "inscriptions" (
+    "id",
+    "ref_user",
+    "ref_event"
+) VALUES (
+    1,
+    1,
+    1
+)
+ON CONFLICT DO NOTHING;
+
+INSERT INTO "attendances" (
+    "id",
+    "ref_inscription"
+) VALUES (
+    1,
+    1
+);
