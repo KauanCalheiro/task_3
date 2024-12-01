9cfe99e89392b1c358413ca97401936799fe5af2
joao.vieceli <joao.vieceli@universo.univates.br>
2024-12-01 00:14:59 -0300

    feat: Adicionando Cors na Sync

--- sync/app/Http/Middleware/TrustKey.php
+++ sync/app/Http/Middleware/TrustKey.php
@@ -40,7 +40,11 @@ class TrustKey
             }
 
             $response = $next($request);
-            
+
+            $response->headers->set('Access-Control-Allow-Origin', '*');
+            $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+            $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization, Accept, Origin, X-CSRF-Token');
+
             return $response;
         }
         catch (Exception $e) {
